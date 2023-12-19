5b60e693409b32c5dc0e59d6986b06820200606e
phu318 <baonguyen3181999@gmail.com>
2023-12-19 09:53:44 +0700

    tsss

---
+++ Documents/Md2/LuyenTap2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bạn vui lòng nhập ngày");
+        long day = scanner.nextLong();
+        System.out.println("Bạn vui lòng nhập tháng");
+        long month = scanner.nextLong();
+        System.out.println("Bạn vui lòng nhập năm");
+        long year = scanner.nextLong();
+    }
+}
---
+++ Documents/Md2/LuyenTap2/src/MiniTest.java
@@ -0,0 +1,38 @@
+public class MiniTest {
+    private long day;
+    private long month;
+    private long year;
+
+    public MiniTest() {
+    }
+
+    public MiniTest(long day, long month, long year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public long getDay() {
+        return day;
+    }
+
+    public void setDay(long day) {
+        this.day = day;
+    }
+
+    public long getMonth() {
+        return month;
+    }
+
+    public void setMonth(long month) {
+        this.month = month;
+    }
+
+    public long getYear() {
+        return year;
+    }
+
+    public void setYear(long year) {
+        this.year = year;
+    }
+}
